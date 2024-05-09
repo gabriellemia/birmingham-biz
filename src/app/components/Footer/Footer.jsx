c75c9d547624a3a25dfe8cde1ee83eb07aced98b
chari00 <macharitocruzuk@gmail.com>
2024-05-09 18:42:36 +0100

    add route to contact page,add social links, arial label and further styling.

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -4,32 +4,61 @@ import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-// import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className={styles.footerContainer}>
       <ul className={styles.footerMenu}>
-        <li className={styles.listItem}>Contact Us</li>
-        <li className={styles.listItem}>Privacy policy</li>
+        <li className={styles.listItem}>
+          <Link href="/contact" className={styles.link}>
+            Contact Us
+          </Link>
+        </li>
+        <li className={styles.listItem}>Privacy Policy</li>
       </ul>
-      <small>
-        <p className={styles.copyright}>© All Content Birmingham Business</p>
-      </small>
+
+      <p className={styles.copyright}>
+        <small>© All Content Birmingham Business</small>
+      </p>
 
       <section className={styles.footerSocial}>
         <ul className={styles.socialMenu}>
           <li className={styles.socialItem}>
-            <FontAwesomeIcon icon={faLinkedin} />
+            <a
+              href="https://www.linkedin.com/company/birmingham-business/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+            </a>
           </li>
           <li className={styles.socialItem}>
-            <FontAwesomeIcon icon={faInstagram} />
+            <a
+              href="https://www.instagram.com/birminghambiz/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+            </a>
           </li>
           <li className={styles.socialItem}>
-            <FontAwesomeIcon icon={faXTwitter} />
+            <a
+              href="https://twitter.com/brumbiz"
+              target="_blank"
+              aria-label="XTwitter"
+            >
+              <FontAwesomeIcon icon={faXTwitter} className={styles.icon} />
+            </a>
           </li>
           <li className={styles.socialItem}>
-            <FontAwesomeIcon icon={faFacebook} />
+            <a
+              href="https://www.facebook.com/brumbiz"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
+            </a>
           </li>
         </ul>
       </section>
