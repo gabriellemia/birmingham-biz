a5ec67d1ec99289aae6c5c10ded9c703656754ad
chari00 <macharitocruzuk@gmail.com>
2024-05-09 11:51:06 +0100

    add social icons

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,19 +1,35 @@
-import styles from './Footer.module.css'
-
+import styles from "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+// import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>
-      <ul className={styles.list-menu}>
-        <li className={styles.list-item}>Contact Us</li>
-        <li className={styles.list-item}>Privacy policy</li>
+    <div className={styles.footerContainer}>
+      <ul className={styles.footerMenu}>
+        <li className={styles.listItem}>Contact Us</li>
+        <li className={styles.listItem}>Privacy policy</li>
       </ul>
-      <small><p>© All Content Birmingham Business</p></small>
-      
-      
+      <small>
+        <p>© All Content Birmingham Business</p>
+      </small>
 
+      <div className={styles.footerSocial}>
+        <ul>
+          <li><FontAwesomeIcon icon={faLinkedin} /></li>
+          <li><FontAwesomeIcon icon={faInstagram}/> </li>
+          <li><FontAwesomeIcon icon={faXTwitter}/> </li>
+          <li><FontAwesomeIcon icon={faFacebook}/> </li>
 
+        </ul>
+
+      </div>
+
+      
+   
     </div>
-
-  )
+  );
 }
