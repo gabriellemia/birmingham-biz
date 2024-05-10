ad71061165d94eb4ec3ed792ba81859716ff9a99
chari00 <macharitocruzuk@gmail.com>
2024-05-10 14:41:53 +0100

    add news section route to home page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import NewsSection from "./components/NewsSection/NewsSection";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello World!</h1>
+      <NewsSection/>
     </main>
   );
 }
