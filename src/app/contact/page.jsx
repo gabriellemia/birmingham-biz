23c8894865701e33d59cc2da089a25a45e5529e7
Josh Foston <jfoston90@gmail.com>
2024-05-12 13:26:07 +0100

    alighn the text correctly, added the cityscape image to the background

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,5 @@
 import styles from './page.module.css'
+import Image from 'next/image'
 
 export default function Contact() {
   return (
@@ -7,6 +8,12 @@ export default function Contact() {
     <main className={styles.mainPage}>
     <p className={styles.contactText}>Birmingham Business is an editorially-led magazine and we are keen to hear from you, though the inclusion or articles or comment columns cannot be guaranteed and is always at the Editor’s discretion.</p>
     </main>
+    <Image className={styles.contactImage}
+          src="/birmingham_background.svg"
+          width={1440} height={556}
+          alt="Birmingham Cityscape"
+      />
     </div>
+    
   )
 }
