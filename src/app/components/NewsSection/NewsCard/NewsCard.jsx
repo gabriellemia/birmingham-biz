5e19b9faf4dde201cc82f5a2b4b6a69cf5bba143
1ravi48 <ravi48@hotmail.co.uk>
2024-05-13 12:25:52 +0100

    newsCard section progressed

--- src/app/components/NewsSection/NewsCard/NewsCard.jsx
+++ src/app/components/NewsSection/NewsCard/NewsCard.jsx
@@ -1,16 +1,16 @@
+import Image from "next/image";
+import styles from "./NewsCard.module.css";
 
-import Image from 'next/image'
-import styles from './NewsCard.css'
-
-const NewsCard = ({imgUrl, headline, description}) => {
+const NewsCard = ({ imgUrl, headline, description }) => {
   return (
-    <section className={styles.cardContainer} >
-       <Image src={imgUrl} alt={headline} width={300} height={200}/>
-       <h2 className={styles.headline}>{headline} headline title</h2>
+    <section className={styles.cardContainer}>
+      <Image src={imgUrl} alt={headline} width={390} height={292} />
+      <div className={styles.newsCardText}>
+        <h2 className={styles.headline}>{headline} headline title</h2>
         <p className={styles.description}>{description} description/content </p>
-      
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default NewsCard
+export default NewsCard;
