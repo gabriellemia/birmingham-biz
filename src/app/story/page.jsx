b3d93d502e80c44d267427654739c2051379f87f
gaby mia <78322726+gabriellemia@users.noreply.github.com>
2024-05-13 10:30:36 +0100

    figuring out stories page

--- src/app/story/page.jsx
+++ src/app/story/page.jsx
@@ -1,7 +1,23 @@
-import styles from './page.module.css'
+"use client";
+import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+
+require("dotenv").config();
+
+const TOKEN = process.env.API_TOKEN;
 
 export default function Story() {
-  return (
-    <h1>Story</h1>
-  )
+
+
+    useEffect(() => {
+    fetch("https://strapi-production-9d37.up.railway.app/api/articles", {
+      headers: {
+        authorization: `Bearer ${TOKEN}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data.data[2].attributes);
+  }
+
+  return <h1>Story</h1>;
 }
