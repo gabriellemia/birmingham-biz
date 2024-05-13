399458a44f17afc99588c30ced47d6f42f737b5b
gaby mia <78322726+gabriellemia@users.noreply.github.com>
2024-05-13 20:05:41 +0100

    configured request file to access data

--- src/app/request/request.js
+++ src/app/request/request.js
@@ -1,16 +1,19 @@
 "use client";
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+// require("dotenv").config();
 
-const TOKEN = process.env.API_TOKEN;
+const TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
 
 async function getArticles() {
   const response = await fetch("https://strapi-production-9d37.up.railway.app/api/articles", {
     headers: {
-      authorization: `Bearer ${TOKEN}`,
+      Authorization: `Bearer ${TOKEN}`,
+      "Content-Type": "application/json",
     },
   });
   const data = await response.json();
-  console.log(data.data[0].attributes);
+  console.log(data.data[1].attributes.copy);
 }
 
 getArticles();
