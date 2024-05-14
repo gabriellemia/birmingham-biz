0583abe13137f6ce5d8cde3faa80784ea7d7c748
chari00 <macharitocruzuk@gmail.com>
2024-05-14 10:22:09 +0100

    add aria labels for accessibility

--- src/app/components/MostPopularAndBBTV/BBTV/BBTV.jsx
+++ src/app/components/MostPopularAndBBTV/BBTV/BBTV.jsx
@@ -4,7 +4,12 @@ import styles from "./BBTV.module.css";
 
 const videoUrl = "https://player.vimeo.com/video/889425912?color&autopause=0&loop=0&muted=0&title=1&portrait=1&byline=1&h=88c00148cf#t=";
 
-export default function BBTV({ episodeNumber, date, description }) {
+const handleWatch = ()=>{
+  <a href="https://bbtv.live/"></a>
+}
+
+
+export default function BBTV({ episodeNumber, date, description, videoTitle  }) {
 
   return (
     <section className={styles.BBTVContainer}>
@@ -12,25 +17,26 @@ export default function BBTV({ episodeNumber, date, description }) {
       
       <iframe
         src={videoUrl}
+        title={videoTitle}
         width="675"
         height="365"
         frameborder="0"
-        allow="fullscreen"
         allowfullscreen
       ></iframe>
 
       <section className={styles.content}>
-        <h2 className={styles.episodeTitle}>Episode 10{episodeNumber}</h2>
+        <h2 className={styles.episodeTitle} aria-live="polite">Episode 10{episodeNumber}</h2>
         <p className={styles.date}>{date}Friday 1st December, 2023</p>
         <p className={styles.description}>
           {description} Some information about the episode, key stories, key
           guests and things.
         </p>
-        <p className={styles.watchNow}>
+        <p className={styles.watchNow} onClick={''}>
           Watch now{" "}
           <FontAwesomeIcon
             icon={faArrowRight}
             className={styles.arrowIcon}
+            aria-hidden="true" // Hide decorative icons from screen readers
           ></FontAwesomeIcon>
         </p>
       </section>
