b6ee37a859e39f20ce359d33b2359a3c2b9c82b5
Josh Foston <jfoston90@gmail.com>
2024-05-15 15:33:29 +0100

    aligned the nav text correctly and added the header background

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -5,53 +5,56 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <Image
-        className={styles.logoBB}
-        src="/BBLogo.png"
-        width={147}
-        height={150}
-        alt="Birmingham Business logo"
-      />
-      <nav className={styles.topNavBar}>
-        <h1 className={styles.birmingham}>Birmingham</h1>
-        <ul className={styles.navBarContainer}>
-          <li>
-            <a className={styles.navLinks} href="/">
-              Home
-            </a>
-          </li>
-          <li>
-            <a className={styles.navLinks} href="/story">
-              News
-            </a>
-          </li>
-          <li>
-            <a className={styles.navLinks} href="/story">
-              Features and Analysis
-            </a>
-          </li>
-          <li>
-            <a className={styles.navLinks} href="/story">
-              Five Mintues With
-            </a>
-          </li>
-          <li>
-            <a className={styles.navLinks} href="/story">
-              Insight
-            </a>
-          </li>
-          <li>
-            <a className={styles.navLinks} href="/story">
-              Magazine Archive
-            </a>
-          </li>
-        </ul>
-        {/* <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.magIcon} /> */}
-      </nav>
-      <nav className={styles.bottomNavBar}>
-        <h1 className={styles.business}>Business</h1>
-      </nav>
-    </header>
+    <>
+      <div className={styles.headerBackground}></div>
+      <header className={styles.header}>
+        <Image
+          className={styles.logoBB}
+          src="/BBLogo.png"
+          width={147}
+          height={150}
+          alt="Birmingham Business logo"
+        />
+        <nav className={styles.topNavBar}>
+          <h1 className={styles.birmingham}>Birmingham</h1>
+          <ul className={styles.navBarContainer}>
+            <li>
+              <a className={styles.navLinks} href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/story">
+                News
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/story">
+                Features and Analysis
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/story">
+                Five Mintues With
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/story">
+                Insight
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/story">
+                Magazine Archive
+              </a>
+            </li>
+          </ul>
+          {/* <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.magIcon} /> */}
+        </nav>
+        <nav className={styles.bottomNavBar}>
+          <h1 className={styles.business}>Business</h1>
+        </nav>
+      </header>
+    </>
   );
 }
