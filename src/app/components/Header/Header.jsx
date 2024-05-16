03e0a8ae8f4610375a8d2b9f29ac707e26bb1b8e
Josh Foston <jfoston90@gmail.com>
2024-05-16 14:14:24 +0100

    header is at MVP1 stage

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -7,82 +7,82 @@ import { FaBars, FaTimes, faBars, faTimes } from "react-icons/fa";
 import { useRef } from "react";
 
 export default function Header() {
-	const navRef = useRef();
+  const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(styles.responsive_nav);
-	};
+  const showNavbar = () => {
+    navRef.current.classList.toggle(styles.responsive_nav);
+  };
 
-	return (
-		<>
-			<div className={styles.headerBackground}></div>
-			<header className={styles.header}>
-				<Image
-					className={styles.logoBB}
-					src="/BBLogo.png"
-					width={147}
-					height={150}
-					alt="Birmingham Business logo"
-				/>
-				<nav className={styles.topNavBar}>
-					<h1 className={styles.birmingham}>Birmingham</h1>
-					<ul className={styles.navBarContainer} ref={navRef}>
-						<li>
-							<a className={styles.navLinks} href="/">
-								Home
-							</a>
-						</li>
-						<li>
-							<a className={styles.navLinks} href="/story">
-								News
-							</a>
-						</li>
-						<li>
-							<a className={styles.navLinks} href="/story">
-								Features and Analysis
-							</a>
-						</li>
-						<li>
-							<a className={styles.navLinks} href="/story">
-								Five Mintues With
-							</a>
-						</li>
-						<li>
-							<a className={styles.navLinks} href="/story">
-								Insight
-							</a>
-						</li>
-						<li>
-							<a className={styles.navLinks} href="/story">
-								Magazine Archive
-							</a>
-						</li>
-						<button
-							className={`${styles.navBtn} ${styles.navCloseBtn}`}
-							onClick={showNavbar}
-						>
-							<FaTimes />
-						</button>
-					</ul>
+  return (
+    <>
+      <div className={styles.headerBackground}></div>
+      <header className={styles.header}>
+        <Image
+          className={styles.logoBB}
+          src="/BBLogo.png"
+          width={147}
+          height={150}
+          alt="Birmingham Business logo"
+        />
+        <nav className={styles.topNavBar}>
+          <h1 className={styles.birmingham}>Birmingham</h1>
+          <ul className={styles.navBarContainer} ref={navRef}>
+            <li>
+              <a className={styles.navLinks} href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/story">
+                News
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/story">
+                Features and Analysis
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/story">
+                Five Mintues With
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/story">
+                Insight
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/story">
+                Magazine Archive
+              </a>
+            </li>
+            <button
+              className={`${styles.navBtn} ${styles.navCloseBtn} ${styles.navCross}`}
+              onClick={showNavbar}
+            >
+              <FaTimes />
+            </button>
+          </ul>
 
-					{/* <div className={styles.magContainer}>
+          {/* <div className={styles.magContainer}>
 						<FontAwesomeIcon
 							icon={faMagnifyingGlass}
 							className={styles.magIcon}
 						/>
 					</div> */}
-					<button
-						className={`${styles.navBtn} ${styles.navCloseBtn}`}
-						onClick={showNavbar}
-					>
-						<FaBars />
-					</button>
-				</nav>
+          <button
+            className={`${styles.navBtn} ${styles.navCloseBtn} ${styles.burgerIcon}`}
+            onClick={showNavbar}
+          >
+            <FaBars />
+          </button>
+        </nav>
 
-				<nav className={styles.bottomNavBar}>
-					<h1 className={styles.business}>Business</h1>
-				</nav>
-			</header>
-		</>
-	);
+        <nav className={styles.bottomNavBar}>
+          <h1 className={styles.business}>Business</h1>
+        </nav>
+      </header>
+    </>
+  );
 }
