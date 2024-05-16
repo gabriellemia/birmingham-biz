04a8fa457de8a465f88e6ac909279e3e8ba95119
Erin Shien <erinshiensmith@gmail.com>
2024-05-16 14:33:50 +0100

    rewrote API endpoints for different sections

--- src/app/request/request.js
+++ src/app/request/request.js
@@ -8,7 +8,103 @@ const TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
 export async function getArticles() {
   try {
     const response = await fetch(
-      "https://strapi-production-9d37.up.railway.app/api/articles/?populate=image",
+      "https://strapi-production-9d37.up.railway.app/api/articles/?populate[0]=image&populate[1]=ad1&populate[2]=ad2&populate[3]=ad3&populate[4]=ad4&populate[5]=ad5",
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching articles:", error.message);
+    return null;
+  }
+}
+
+export async function getNewsArticles() {
+  try {
+    const response = await fetch(
+      "https://strapi-production-9d37.up.railway.app/api/news/?populate[0]=image&populate[1]=ad1&populate[2]=ad2&populate[3]=ad3&populate[4]=ad4&populate[5]=ad5",
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching articles:", error.message);
+    return null;
+  }
+}
+
+export async function getFeaturesArticles() {
+  try {
+    const response = await fetch(
+      "https://strapi-production-9d37.up.railway.app/api/features/?populate[0]=image&populate[1]=ad1&populate[2]=ad2&populate[3]=ad3&populate[4]=ad4&populate[5]=ad5",
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching articles:", error.message);
+    return null;
+  }
+}
+
+export async function getFiveArticles() {
+  try {
+    const response = await fetch(
+      "https://strapi-production-9d37.up.railway.app/api/five-minutes-withs/?populate[0]=image&populate[1]=ad1&populate[2]=ad2&populate[3]=ad3&populate[4]=ad4&populate[5]=ad5",
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching articles:", error.message);
+    return null;
+  }
+}
+
+export async function getInsightsArticles() {
+  try {
+    const response = await fetch(
+      "https://strapi-production-9d37.up.railway.app/api/insights/?populate[0]=image&populate[1]=ad1&populate[2]=ad2&populate[3]=ad3&populate[4]=ad4&populate[5]=ad5",
       {
         headers: {
           Authorization: `Bearer ${TOKEN}`,
