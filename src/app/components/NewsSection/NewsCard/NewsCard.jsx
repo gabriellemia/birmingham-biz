9b4ca9f81e0682b231c2101c77033a5dfc41b6d6
1ravi48 <ravi48@hotmail.co.uk>
2024-05-17 14:53:39 +0100

    media queries progressed on homeapge - BBTV and most popular sections

--- src/app/components/NewsSection/NewsCard/NewsCard.jsx
+++ src/app/components/NewsSection/NewsCard/NewsCard.jsx
@@ -4,13 +4,15 @@ import styles from "./NewsCard.module.css";
 const NewsCard = ({ imgUrl, headline, description }) => {
   return (
     <section className={styles.cardContainer}>
-      <Image
-        src={imgUrl}
-        alt={headline}
-        width={290}
-        height={272}
-        className={styles.imgStyle}
-      />
+      <div className={styles.imgDiv}>
+        <Image
+          src={imgUrl}
+          alt={headline}
+          size="max-width: 100%"
+          fill={true}
+          className={styles.imgStyle}
+        />
+      </div>
       <div className={styles.newsCardText}>
         <h2 className={styles.headline}>{headline}</h2>
         <p className={styles.description}>{description}</p>
