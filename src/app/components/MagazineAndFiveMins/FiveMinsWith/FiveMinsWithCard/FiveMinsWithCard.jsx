62feb3a47cc01f9c1b4734f020f6ab25e9af2f59
chari00 <macharitocruzuk@gmail.com>
2024-05-17 13:25:45 +0100

    add semantic html.

--- src/app/components/MagazineAndFiveMins/FiveMinsWith/FiveMinsWithCard/FiveMinsWithCard.jsx
+++ src/app/components/MagazineAndFiveMins/FiveMinsWith/FiveMinsWithCard/FiveMinsWithCard.jsx
@@ -3,12 +3,20 @@ import Image from "next/image";
 
 export default function MostPopularCard({ imgUrl, headline, description }) {
   return (
-    <div className={styles.card}>
-      <Image src={imgUrl} alt={headline} width={190} height={140} className={styles.imgStyle} />
+    <article className={styles.card}>
+      <figure className={styles.imageContainer}>
+        <Image
+          src={imgUrl}
+          alt={headline}
+          width={190}
+          height={140}
+          className={styles.imgStyle}
+        />
+      </figure>{" "}
       <div className={styles.cardText}>
         <h1 className={styles.headline}>{headline}</h1>
         <p className={styles.description}>{description}</p>
       </div>
-    </div>
+    </article>
   );
 }
