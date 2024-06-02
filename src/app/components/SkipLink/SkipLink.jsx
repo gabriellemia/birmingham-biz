b6813be9fad9b3c61c6ad20c8d6f62954651c9e2
chari00 <macharitocruzuk@gmail.com>
2024-06-02 18:39:40 +0100

    add skiplink component for screen reader accessibility.

---
+++ src/app/components/SkipLink/SkipLink.jsx
@@ -0,0 +1,12 @@
+
+import styles from "./SkipLink.module.css";
+
+const SkipLink = () => {
+  return (
+    <a href="#maincontent" className={styles.skipLink}>
+      Skip to Main Content
+    </a>
+  );
+};
+
+export default SkipLink;
